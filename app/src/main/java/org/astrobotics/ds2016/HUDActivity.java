a5f4ef3e155dfdb9619565f1c31c725982d6abc3
Anirudh Bagde <anidev.aelico@gmail.com>
2015-11-07 17:29:38 -0500

    Improved layout and added robot and controller status indicators

--- app/src/main/java/org/astrobotics/ds2016/HUDActivity.java
+++ app/src/main/java/org/astrobotics/ds2016/HUDActivity.java
@@ -1,7 +1,14 @@
 package org.astrobotics.ds2016;
 
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
+import android.hardware.input.InputManager;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.InputDevice;
 import android.view.View;
 
 public class HUDActivity extends AppCompatActivity {
@@ -10,6 +17,31 @@ public class HUDActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_hud);
+
+        // Initialize indicators
+        initIndicator(R.id.robot_status, R.drawable.ic_robot_status);
+        initIndicator(R.id.controller_status, R.drawable.ic_controller_status);
+
+        // Register input device listener
+        InputManager inputManager = (InputManager)getApplicationContext().getSystemService(Context.INPUT_SERVICE);
+        inputManager.getInputDeviceIds(); // required for the device listener to be registered
+        inputManager.registerInputDeviceListener(new InputManager.InputDeviceListener() {
+            @Override
+            public void onInputDeviceAdded(int deviceId) {
+                updateGamepadStatus();
+            }
+
+            @Override
+            public void onInputDeviceRemoved(int deviceId) {
+                updateGamepadStatus();
+            }
+
+            @Override
+            public void onInputDeviceChanged(int deviceId) {
+                updateGamepadStatus();
+            }
+        }, null);
+        updateGamepadStatus();
     }
 
     @Override
@@ -17,6 +49,7 @@ public class HUDActivity extends AppCompatActivity {
         super.onPostCreate(savedInstanceState);
     }
 
+    @TargetApi(Build.VERSION_CODES.KITKAT)
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
@@ -28,5 +61,39 @@ public class HUDActivity extends AppCompatActivity {
             flags |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
         }
         getWindow().getDecorView().setSystemUiVisibility(flags);
+    }
+
+    // Sets indicator icon
+    @SuppressWarnings("deprecation")
+    private void initIndicator(int viewId, int iconId) {
+        LayerDrawable layers = (LayerDrawable)findViewById(viewId).getBackground();
+        layers.setDrawableByLayerId(R.id.indicator_icon, getResources().getDrawable(iconId));
+    }
+
+    // Change background color of indicator shape
+    private void setIndicator(int viewId, boolean activated) {
+        LayerDrawable layers = (LayerDrawable)findViewById(viewId).getBackground();
+        Drawable shape = layers.findDrawableByLayerId(R.id.indicator_bg);
+        if(activated) {
+            shape.setLevel(1);
+        } else {
+            shape.setLevel(0);
+        }
+    }
+
+    // Update gamepad status indicator
+    private void updateGamepadStatus() {
+        int gamepadCheck = InputDevice.SOURCE_GAMEPAD
+                         | InputDevice.SOURCE_JOYSTICK;
+//                         | InputDevice.SOURCE_DPAD;
+        for(int id : InputDevice.getDeviceIds()) {
+            InputDevice device = InputDevice.getDevice(id);
+            int sources = device.getSources();
+            if((sources & gamepadCheck) == gamepadCheck) {
+                setIndicator(R.id.controller_status, true);
+                return;
+            }
+        }
+        setIndicator(R.id.controller_status, false);
     }
 }
